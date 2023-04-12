6bab46b2547834f492d03beb8404799c498d522a
Ilija <68541501+ikarpushyn@users.noreply.github.com>
2023-04-12 20:48:16 +0300

    create server

--- index.js
+++ index.js
@@ -1,12 +1,29 @@
 import express from 'express';
+import { userRouter } from './users/users.js';
 
 const port = 8000;
+
 const app = express();
 
+app.use((req, res, next) => {
+	console.log(`Time , ${Date.now()}`);
+	next();
+});
+
 app.get('/hello', (req, res) => {
-	res.send('HELLO');
+	// res.send('HELLO');
+
+	throw new Error('Errorssss');
+});
+
+app.use('/users', userRouter);
+
+app.use((err, req, res, next) => {
+	console.log(err.message);
+	res.status(500).send(err.message);
+	next();
 });
 
 app.listen(port, () => {
-	console.log(`Server started on http://localhost:8000${port}`);
+	console.log(`Server started on http://localhost:${port}`);
 });
